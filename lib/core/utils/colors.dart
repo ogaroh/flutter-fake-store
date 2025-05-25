beb30f175d1e6a6f48d79a9822595e1b495cebe4
Erick Ogaro <ogaroerick1998@gmail.com>
2025-05-26 00:22:13 +0300

    utils

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+final Color defaultDark = Color(0xFF1E232C);
+final Color defaultBlack = Color(0xFF1E1E1E);
+final Color defaultWhite = Color(0xFFFFFFFF);
+final Color defaultGrey = Color(0xFF808080);
+final Color defaultRed = Color(0xFFFF0000);
+final Color defaultGreen = Color(0xFF00FF00);
+final Color defaultBlue = Color(0xFF0000FF);
+final Color defaultYellow = Color(0xFFFFFF00);
+final Color defaultPurple = Color(0xFF800080);
+final Color defaultOrange = Color(0xFFFFA500);
+final Color lightOrange = Color(0xFFFFE8B2);
