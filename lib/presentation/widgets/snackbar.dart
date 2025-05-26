de774ce3d4317d38be9b83f56bba857e4e6c8137
Erick Ogaro <ogaroerick1998@gmail.com>
2025-05-26 13:46:35 +0300

    Enhance CustomSnackbar by adding styling options including text color, background color, and rounded corners for improved visibility and user experience.

--- lib/presentation/widgets/snackbar.dart
+++ lib/presentation/widgets/snackbar.dart
@@ -1,4 +1,5 @@
 // show snackbar with a message
+import 'package:fake_store/core/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomSnackbar {
@@ -6,7 +7,15 @@ class CustomSnackbar {
     // close all other snackbars
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message), duration: Duration(seconds: 1)),
+      SnackBar(
+        content: Text(
+          message,
+          style: TextStyle(color: AppColors.white, fontWeight: FontWeight.bold),
+        ),
+        duration: Duration(seconds: 1),
+        backgroundColor: AppColors.dark,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
     );
   }
 }
